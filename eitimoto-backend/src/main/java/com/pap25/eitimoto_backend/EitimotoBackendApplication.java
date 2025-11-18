cf0f7853438643394d903ef91aadd04b5fcef25c
riko111 <adrianoskar007@gmail.com>
2025-11-18 18:19:03 +0100

    initialized backend

---
+++ eitimoto-backend/src/main/java/com/pap25/eitimoto_backend/EitimotoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pap25.eitimoto_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EitimotoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EitimotoBackendApplication.class, args);
+	}
+
+}
