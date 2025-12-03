aadedec89cc1cbfaff1742d06bf2526fd330cc37
julianjarosz <julek.jarosz123@gmail.com>
2025-12-03 22:24:11 +0100

    Deadline changes

--- eitimoto-backend/src/main/java/com/pap25/eitimoto_backend/services/JWTService.java
+++ eitimoto-backend/src/main/java/com/pap25/eitimoto_backend/services/JWTService.java
@@ -15,9 +15,8 @@ import java.util.function.Function;
 
 @Service
 public class JWTService {
-    // Example secret key (in a real application, store this securely and do not hardcode)
     private static final String SECRET_KEY = "404E635266556A586E3272357538782F413F4428472B4B6250645367566B5970";
-    
+
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
     }
@@ -31,7 +30,7 @@ public class JWTService {
                 .setClaims(extraClaims)
                 .setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24)) // Token ważny 24h
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24))
                 .signWith(getSignInKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
