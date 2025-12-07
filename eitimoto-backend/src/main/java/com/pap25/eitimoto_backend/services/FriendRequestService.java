5c7eac276c695cdffbf3ba7f62a607e4bc459378
Patryk Nisgorski <patryknisgorski@gmail.com>
2025-12-07 03:30:46 +0100

    no change

--- eitimoto-backend/src/main/java/com/pap25/eitimoto_backend/services/FriendRequestService.java
+++ eitimoto-backend/src/main/java/com/pap25/eitimoto_backend/services/FriendRequestService.java
@@ -63,6 +63,7 @@ public class FriendRequestService {
                                     .build();
                         }
 
+
                     })
                     .collect(Collectors.toList());
         }
