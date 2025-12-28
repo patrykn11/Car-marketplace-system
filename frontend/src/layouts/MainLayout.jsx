b44306e9a0eaff60787d703a39fcab5b04581d6a
pawel kozikowski <pawelkozikowski58@gmail.com>
2025-12-28 12:15:29 +0100

    Fixed Navbar

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -5,9 +5,8 @@ import Footer from '../components/Footer';
 const MainLayout = () => {
     return (
         <div className="flex flex-col min-h-screen bg-gray-50 dark:bg-gray-900 text-gray-900 dark:text-gray-100 transition-colors duration-300">
-            
             <Navbar />
-            <main className="flex-grow w-full">
+            <main className="flex-grow w-full pt-16">
                 <Outlet />
             </main>
             
