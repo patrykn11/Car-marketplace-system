ee4250f4d336d259e7dbefd521c5712b3a360884
julianjarosz <julek.jarosz123@gmail.com>
2026-01-12 00:39:24 +0100

    Fix audi logo

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -13,7 +13,7 @@ const BODY_TYPES = [
 
 const BRAND_ADS = [
     { id: 1, name: "BMW", slogan: "Sheer Driving Pleasure", logo: "https://upload.wikimedia.org/wikipedia/commons/4/44/BMW.svg", link: "/cars?brand=BMW", theme: "from-blue-600 to-blue-800" },
-    { id: 2, name: "Audi", slogan: "Vorsprung durch Technik", logo: "https://upload.wikimedia.org/wikipedia/commons/6/6f/Audi_logo_detail.svg", link: "/cars?brand=Audi", theme: "from-gray-800 to-gray-900" },
+    { id: 2, name: "Audi", slogan: "Vorsprung durch Technik", logo: "https://logowik.com/content/uploads/images/562_audi.jpg", link: "/cars?brand=Audi", theme: "from-gray-800 to-gray-900" },
     { id: 3, name: "Mercedes-Benz", slogan: "The Best or Nothing", logo: "https://upload.wikimedia.org/wikipedia/commons/9/90/Mercedes-Logo.svg", link: "/cars?brand=Mercedes", theme: "from-black to-gray-800" },
     { id: 4, name: "Tesla", slogan: "Electric Future", logo: "https://upload.wikimedia.org/wikipedia/commons/b/bd/Tesla_Motors.svg", link: "/cars?brand=Tesla", theme: "from-red-600 to-red-800" },
     { id: 5, name: "Toyota", slogan: "Let's Go Places", logo: "https://upload.wikimedia.org/wikipedia/commons/9/9d/Toyota_carlogo.svg", link: "/cars?brand=Toyota", theme: "from-red-500 to-red-700" }
