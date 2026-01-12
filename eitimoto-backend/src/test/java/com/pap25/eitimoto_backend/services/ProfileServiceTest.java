3a21091211cb0f2fbd14119bbb496f037d84276a
julianjarosz <julek.jarosz123@gmail.com>
2026-01-13 00:29:41 +0100

    Intentionally failed a test, to check whether CICD works

--- eitimoto-backend/src/test/java/com/pap25/eitimoto_backend/services/ProfileServiceTest.java
+++ eitimoto-backend/src/test/java/com/pap25/eitimoto_backend/services/ProfileServiceTest.java
@@ -57,7 +57,7 @@ class ProfileServiceTest {
 
         // Assert
         assertEquals(username, result.getUsername());
-        assertEquals("test@test.com", result.getEmail());
+        assertEquals("test@test.meow", result.getEmail());
     }
 
     @Test
