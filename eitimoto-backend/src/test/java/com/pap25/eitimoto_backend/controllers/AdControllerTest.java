7351dc3c40b5d7fb38905092c42b61deee7f31f7
julianjarosz <julek.jarosz123@gmail.com>
2026-01-14 21:12:22 +0100

    Fixed adcontrollertest

--- eitimoto-backend/src/test/java/com/pap25/eitimoto_backend/controllers/AdControllerTest.java
+++ eitimoto-backend/src/test/java/com/pap25/eitimoto_backend/controllers/AdControllerTest.java
@@ -1,11 +1,16 @@
 package com.pap25.eitimoto_backend.controllers;
 
 import com.pap25.eitimoto_backend.services.AdService;
+import com.pap25.eitimoto_backend.services.JWTService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.test.web.servlet.MockMvc;
+import com.pap25.eitimoto_backend.config.JWTAuthenticationFilter;
 
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
@@ -14,10 +19,24 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(AdController.class)
+@AutoConfigureMockMvc(addFilters = false)
 class AdControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+
+    @MockBean
+    private JWTService jwtService;
+
+    @MockBean
+    private UserDetailsService userDetailsService;
+
+    @MockBean
+    private JWTAuthenticationFilter jwtAuthenticationFilter;
+
+    @MockBean
+    private AuthenticationProvider authenticationProvider;
 
     @MockBean
     private AdService adService;
@@ -27,10 +46,10 @@ class AdControllerTest {
         String expectedBrand = "BMW";
         given(adService.favoriteCar()).willReturn(expectedBrand);
 
-        mockMvc.perform(get("/api/Ad"))                 
-                .andExpect(status().isOk())             
-                .andExpect(content().string(expectedBrand)); 
-        
-        verify(adService).favoriteCar(); 
+        mockMvc.perform(get("/api/Ad"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(expectedBrand));
+
+        verify(adService).favoriteCar();
     }
 }
