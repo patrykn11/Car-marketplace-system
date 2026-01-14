cad5d45ee4d65228c0614e0c91ba02162cb00ca0
julianjarosz <julek.jarosz123@gmail.com>
2026-01-14 23:34:01 +0100

    final fix in files

--- eitimoto-backend/src/main/java/com/pap25/eitimoto_backend/dto/CarResponseDto.java
+++ eitimoto-backend/src/main/java/com/pap25/eitimoto_backend/dto/CarResponseDto.java
@@ -6,8 +6,6 @@ import lombok.Data;
 @Data
 @Builder
 public class CarResponseDto {
-    // private Long carId;
-    
     private String carBrand;
     private String carModel;
     private Integer productionYear;
